44d9546ad091bdf0ec4552fee3f33641c1f96e2e
Daniel Collin <daniel@collin.com>
2024-07-20 14:10:23 +0200

    wezterm & setup scripts

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,31 @@
+local wezterm = require 'wezterm'
+return {
+	-- color_scheme = 'termnial.sexy',
+	color_scheme = 'Catppuccin Mocha',
+	enable_tab_bar = false,
+	font_size = 16.0,
+	font = wezterm.font('MesloLGM Nerd Font'),
+	
+	window_background_opacity = 1.0,
+	window_decorations = 'RESIZE',
+	keys = {
+		{
+			key = 'f',
+			mods = 'CTRL',
+			action = wezterm.action.ToggleFullScreen,
+		},
+		{
+			key = '\'',
+			mods = 'CTRL',
+			action = wezterm.action.ClearScrollback 'ScrollbackAndViewport',
+		},
+	},
+	mouse_bindings = {
+	  -- Ctrl-click will open the link under the mouse cursor
+	  {
+	    event = { Up = { streak = 1, button = 'Left' } },
+	    mods = 'CTRL',
+	    action = wezterm.action.OpenLinkAtMouseCursor,
+	  },
+	},
+}
